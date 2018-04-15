dfa6f6fa864a2da0c6a05bd209e736edf471a9d3
Remi Gascou <remi@gascou.net>
2018-04-15 09:59:47 +0200

    Added errordocuments Added htaccess Added favicon

---
+++ errordocuments/403.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <title>Login</title>
+    <link rel="icon" href="../meta/favicon_64.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="../styles/errors.css" media="screen">
+</head>
+
+<body>
+    <center>
+        <div class="errordocument-box">
+            <div class="errordocument-box-header">403</div>
+            <div class="errordocument-box-explanation">
+                Access Denied
+            </div>
+        </div>
+    </center>
+</body>
+
+</html>
